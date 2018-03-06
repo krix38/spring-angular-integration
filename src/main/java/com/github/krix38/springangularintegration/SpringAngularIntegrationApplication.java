21abb6c83a9059705b11bb184baa74fca5cc3a4a
Krzysztof Kąkol <krix38@gmail.com>
2018-03-06 11:11:58 +0100

    adding code

---
+++ src/main/java/com/github/krix38/springangularintegration/SpringAngularIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.github.krix38.springangularintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularIntegrationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/krix38/springangularintegration/SpringAngularIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.krix38.springangularintegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAngularIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
